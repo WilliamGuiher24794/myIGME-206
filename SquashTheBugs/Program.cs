336b5a49376c67a6cb5052fccb9fdd4dec7b8ab8
william guiher <guiher.william@yahoo.com>
2020-08-26 17:19:16 -0700

    Squash the Bugs

--- SquashTheBugs/Program.cs
+++ SquashTheBugs/Program.cs
@@ -21,28 +21,34 @@ namespace SquashTheBugs
         {
             // declare int counter
 
-            //int i = 0 Missing a semicolon
+            //int i = 0 
+            //compile error: Missing a semicolon
             int i = 0;
             string allNumbers = null;
+            double number = 0;
 
             // loop through the numbers 1 through 10
             for (i = 1; i < 10; ++i) 
             {
                 // declare string to hold all numbers
-                //string allNumbers = null; move this before the for loop so your not initallizing the string each time through
+                //string allNumbers = null; 
+                // logic error: move this before the for loop so your not initallizing the string each time through
 
                 // output explanation of calculation
 
-                //Console.Write(i + "/" + i - 1 + " = "); need parenthesis around the i - 1
+                //Console.Write(i + "/" + i - 1 + " = "); 
+                //compile error: need parenthesis around the i - 1
                 Console.Write(i + "/" + (i - 1) + " = ");
 
                 // output the calculation based on the numbers
-                //Console.WriteLine(i / (i - 1)); can cause a divide by zero error
+                //Console.WriteLine(i / (i - 1)); 
+                //runtime error: can cause a divide by zero error
 
                 //testing for a divide by zero error if it does it will equal Undefined instead of crashing
                 try
                 {
-                    Console.WriteLine(i / (i - 1));
+                    number = (double)i / (i - 1);
+                    Console.WriteLine(number);
                 }
                 catch
                 {
@@ -50,14 +56,15 @@ namespace SquashTheBugs
                 }
 
                 // concatenate each number to allNumbers
-                allNumbers += i + " ";
+                allNumbers += number + " ";
 
                 // increment the counter
                 i = i + 1;
             }
 
             // output all numbers which have been processed
-            //Console.WriteLine("These numbers have been processed: " allNumbers); need a + between the string and the variable
+            //Console.WriteLine("These numbers have been processed: " allNumbers);
+            //compile error: need a + between the string and the variable
             Console.WriteLine("These numbers have been processed: " + allNumbers);
         }
     }

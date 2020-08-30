2fce3ef5b7be8d378db43988c094c3732175a606
william guiher <guiher.william@yahoo.com>
2020-08-29 19:55:54 -0700

    Created VariableAndExpressions

---
+++ VariableAndExpressions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace VariableAndExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            int num = 0;
+            int total = 1;
+            bool valid;
+            Console.WriteLine("Enter four numbers");
+            for(int i = 0; i < 4; i++)
+            {
+                valid = false;
+                do
+                {
+                    input = Console.ReadLine();
+                    try
+                    {
+                       num = Convert.ToInt32(input);
+                        valid = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Enter a valid number");
+                    }
+                } while (!valid);
+                total *= num;
+            }
+            Console.WriteLine("The product of those four numbers are: " + total);
+        }
+
+    }
+}

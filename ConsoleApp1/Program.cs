80f478d5518424bf3410fcef374f043c39ac9080
william guiher <guiher.william@yahoo.com>
2020-09-16 18:02:34 -0700

    UT1_BugSquash created

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] dArray = new double[2][]; //needs to be a new double[2][];
+            dArray[1] = new double[2];
+            dArray[2] = new double[1]; //needs semicolon
+
+
+            dArray[1][2] = 5.67; //out of bounds error
+
+
+
+        }
+    }
+
+
+}
---
+++ UT1_BugSquash/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace UT1_BugSquash
+{
+    class Program
+    {
+        // Calculate x^y for y > 0 using a recursive function
+        static void Main(string[] args)
+        {
+            string sNumber;
+            int nX;
+            //compile-time error - need semicolon
+            //int nY
+            int nY;
+            int nAnswer;
+
+            //compile-time error - add a quotation in the writeline
+            //Console.WriteLine(This program calculates x ^ y.);
+            Console.WriteLine("This program calculates x ^ y.");
+
+            do
+            {
+                Console.Write("Enter a whole number for x: ");
+                //compile-time error - need to put the value into sNumber
+                //Console.ReadLine();
+                sNumber = Console.ReadLine();
+            } while (!int.TryParse(sNumber, out nX));
+
+            do
+            {
+                Console.Write("Enter a positive whole number for y: ");
+                sNumber = Console.ReadLine();
+            //compile-time error 
+            //} while (int.TryParse(sNumber, out nX));
+            } while (!int.TryParse(sNumber, out nY));
+
+            // compute the factorial of the number using a recursive function
+            nAnswer = Power(nX, nY);
+
+            Console.WriteLine(nX + "^" + nY + " = " + nAnswer);
+        }
+
+        //logical-error - needs to be a static
+        //int Power(int nBase, int nExponent)
+        static int Power(int nBase, int nExponent)
+        {
+            int returnVal = 0;
+            int nextVal = 0;
+
+            // the base case for exponents is 0 (x^0 = 1)
+            if (nExponent == 0)
+            {
+                // return the base case and do not recurse
+                //logical-error - needs to return 1 instead of 0
+                returnVal = 1;
+            }
+            else
+            {
+                // compute the subsequent values using nExponent-1 to eventually reach the base case
+                //locical-error needed it to be -1 so it ends
+                //nextVal = Power(nBase, nExponent + 1);
+                nextVal = Power(nBase, nExponent - 1);
+
+                // multiply the base with all subsequent values
+                returnVal = nBase * nextVal;
+            }
+            //compile error - needs return statment
+            //returnVal;
+            return returnVal;
+        }
+    }
+}
+
